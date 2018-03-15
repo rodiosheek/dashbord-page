41064a61e8aef9437dfa7eec1c8c32128e0d5314
rodiontopagent <rodion@top-agt.com>
2018-03-15 12:39:06 +0200

    add dinamic divider color

--- app/my-app/dashboard-card/dashboard-card.component.js
+++ app/my-app/dashboard-card/dashboard-card.component.js
@@ -4,11 +4,17 @@ require('./dashboard-card.style.scss');
 
 class dashboardCardController {
     constructor() {
-        
+        this.dividerColor = false;
     }
 
     $onInit() {
-        
+        this.__getDividerColor(this.campaign.type);
+    }
+
+    __getDividerColor(type) {
+        if(!type) return;
+        let style = type.split(' ')[0].toLowerCase();
+        this.dividerColor = 'dashboard-card__divider--' + style;
     }
 }
 
