313a6442e5171239a351e98ec47fa460900da989
rodiontopagent <rodion@top-agt.com>
2018-03-15 16:45:51 +0200

    refactored components folders, fix social filter

--- app/my-app/campaigns/campaigns.module.js
+++ app/my-app/campaigns/campaigns.module.js
@@ -1,10 +1,11 @@
 import campaigns from './campaigns.component';
 import campaignsService from './campaigns.service';
-
+import socialNetFilterComponent from './social-network-filter/social-network-filter.component';
 
 require('./campaigns.component.scss');
 
 angular
     .module('myApp.campaigns', ['ngMaterial'])
     .component('campaigns', campaigns)
+    .component('socialFilter', socialNetFilterComponent)
     .factory('campaignsService', campaignsService);
--- app/my-app/dashboard/dashboard.module.js
+++ app/my-app/dashboard/dashboard.module.js
@@ -1,5 +1,7 @@
 import dashboard from './dashboard.component';
 import dashboardService from './dashboard.service';
+import dashboardCardComponent from './dashboard-card/dashboard-card.component';
+import dashboardCardItemComponent from './dashboard-card-item/dashboard-card-item.component';
 
 
 require('./dashboard.component.scss');
@@ -7,4 +9,6 @@ require('./dashboard.component.scss');
 angular
     .module('myApp.dashboard', ['ngMaterial'])
     .component('dashboard', dashboard)
+    .component('dashboardCard', dashboardCardComponent)
+    .component('cardItem', dashboardCardItemComponent)
     .factory('dashboardService', dashboardService);
